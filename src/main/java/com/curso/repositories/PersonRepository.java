85ce16d6348a0ea50a833ff83312421cc1ae884b
LuisFPradoSilva <luisfpradodasilva.profissional@gmail.com>
2024-07-28 14:07:07 -0300

    Criando repositories de Person, ServiceOrder, Technician e Users

---
+++ src/main/java/com/curso/repositories/PersonRepository.java
@@ -0,0 +1,13 @@
+package com.curso.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.curso.domains.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, UUID> {
+
+}
