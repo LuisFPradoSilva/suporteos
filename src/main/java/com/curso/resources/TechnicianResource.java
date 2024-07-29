53f555a61d8e8ecac50a89fd76d421fa2ad131cc
LuisFPradoSilva <luisfelipepradodasilva53@gmail.com>
2024-07-29 20:09:47 -0300

    Implementando funcionalidade de pesquisar por Técnicos pelo ID

--- src/main/java/com/curso/resources/TechnicianResource.java
+++ src/main/java/com/curso/resources/TechnicianResource.java
@@ -1,13 +1,16 @@
 package com.curso.resources;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.curso.domains.Technician;
 import com.curso.domains.dtos.TechnicianDTO;
 import com.curso.services.TechnicianService;
 
@@ -21,5 +24,11 @@ public class TechnicianResource {
     @GetMapping
     public ResponseEntity<List<TechnicianDTO>> findAll() {
         return ResponseEntity.ok().body(techService.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<TechnicianDTO> findById(@PathVariable UUID id) {
+        Technician obj = this.techService.findById(id);
+        return ResponseEntity.ok().body(new TechnicianDTO(obj));
     }
 }
--- src/main/java/com/curso/services/TechnicianService.java
+++ src/main/java/com/curso/services/TechnicianService.java
@@ -1,11 +1,14 @@
 package com.curso.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.curso.domains.Technician;
 import com.curso.domains.dtos.TechnicianDTO;
 import com.curso.repositories.TechnicianRepository;
 
@@ -17,5 +20,10 @@ public class TechnicianService {
 
     public List<TechnicianDTO> findAll() {
         return techRepo.findAll().stream().map(obj -> new TechnicianDTO(obj)).collect(Collectors.toList());
+    }
+
+    public Technician findById(UUID id) {
+        Optional<Technician> obj = techRepo.findById(id);
+        return obj.orElse(null);
     }
 }
