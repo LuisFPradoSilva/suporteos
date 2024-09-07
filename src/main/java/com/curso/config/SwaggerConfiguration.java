36dc353a5155b476aa2bec020d672bab7fff4058
LuisFPradoSilva <luisfelipepradodasilva53@gmail.com>
2024-09-07 16:35:06 -0300

    Programando a classe SwaggerConfiguration

---
+++ src/main/java/com/curso/config/SwaggerConfiguration.java
@@ -0,0 +1,32 @@
+package com.curso.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+            .group("suporteos")
+            .pathsToMatch("/**")
+            .packagesToScan("com.curso.resources")
+            .build();
+    }
+
+    @Bean
+    public io.swagger.v3.oas.models.OpenAPI customOpenAPI() {
+        return new io.swagger.v3.oas.models.OpenAPI().info(new Info().title("SuporteOS")
+            .description("SuporteOS")
+            .version("1.0")
+            .contact(new Contact()
+                .name("Curso Spring")
+                .url("http://www.cursospring.com.br")
+                .email("cursospring@gmail.com")));
+    }
+}
