da7d9bfc2ca573d98e1e6d6c06a355913b8d236a
LuisFPradoSilva <luisfelipepradodasilva53@gmail.com>
2024-09-07 17:06:38 -0300

    Configurando annotations do Swagger na classe ServiceOrderResource

--- src/main/java/com/curso/resources/ServiceOrderResource.java
+++ src/main/java/com/curso/resources/ServiceOrderResource.java
@@ -20,26 +20,33 @@ import com.curso.domains.ServiceOrder;
 import com.curso.domains.dtos.ServiceOrderDTO;
 import com.curso.services.ServiceOrderService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/serviceorder")
+@Tag(name = "Service Order", description = "API para gerenciamento das ordens de serviço")
 public class ServiceOrderResource {
 
     @Autowired
     private ServiceOrderService serviceOrderService;
 
+
     @GetMapping
+    @Operation(summary = "Lista todas as ordens de serviço", description = "Retorna uma lista contendo todas as ordens de serviço cadastradas.")
     public ResponseEntity<List<ServiceOrderDTO>> findAll() {
         return ResponseEntity.ok().body(serviceOrderService.findAll());
     }
 
+    @Operation(summary = "Busca ordem de serviço por ID", description = "Retorna a ordem de serviço correspondente ao ID fornecido.")
     @GetMapping(value = "/{id}")
     public ResponseEntity<ServiceOrderDTO> findById(@PathVariable UUID id) {
         ServiceOrder obj = this.serviceOrderService.findById(id);
         return ResponseEntity.ok().body(new ServiceOrderDTO(obj));
     }
 
+    @Operation(summary = "Cria uma nova ordem de serviço", description = "Cria uma nova ordem de serviço baseada nos dados inseridos.")
     @PostMapping
     public ResponseEntity<ServiceOrderDTO> create(@Valid @RequestBody ServiceOrderDTO objDto) {
         ServiceOrder newObj = serviceOrderService.create(objDto);
@@ -47,12 +54,14 @@ public class ServiceOrderResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @Operation(summary = "Atualiza ordem de serviço", description = "Atualiza os dados cadastrais da ordem de serviço correspondente ao ID fornecido")
     @PutMapping(value = "/{id}")
     public ResponseEntity<ServiceOrderDTO> update(@PathVariable UUID id, @Valid @RequestBody ServiceOrderDTO objDto) {
         ServiceOrder obj = serviceOrderService.update(id, objDto);
         return ResponseEntity.ok().body(new ServiceOrderDTO(obj));
     }
 
+    @Operation(summary = "Deleta ordem de serviço", description = "Exclui a ordem de serviço correspondente ao ID fornecido.")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<ServiceOrderDTO> delete(@PathVariable UUID id) {
         serviceOrderService.delete(id);
