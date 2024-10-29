9e077647c6898ab6875dfb07544463938097d925
LuisFPradoSilva <luisfpradosilva.profissional@proton.me>
2024-10-29 19:51:20 -0300

    Criando a classe UserSS

---
+++ src/main/java/com/curso/security/UserSS.java
@@ -0,0 +1,59 @@
+package com.curso.security;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.curso.domains.Users;
+
+public class UserSS implements UserDetails {
+
+    private String username;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserSS(Users user) {
+        this.username = user.getEmail();
+        this.password = user.getPassword();
+        this.authorities = user.getPersonType().stream().map(x -> new SimpleGrantedAuthority(x.getPersonType()))
+            .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
